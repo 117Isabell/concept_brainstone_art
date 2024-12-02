4b6cedc6c465d8164e56159dd23a890e4fe5e715
117Isabell <isabellhepburn@outlook.com>
2024-12-02 13:23:14 +0100

    feat: add contact info

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,10 @@
+import Contact from "./components/homepage/contact.tsx";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Contact />
+    </div>
+  );
 }
