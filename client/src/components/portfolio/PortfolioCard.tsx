d5a61c2031dfda58917441c1b8723cd99fa6a2d1
tomi <tomislav.varga@icloud.com>
2024-12-03 13:59:36 +0100

    feat: first cards with images

--- client/src/components/portfolio/PortfolioCard.tsx
+++ client/src/components/portfolio/PortfolioCard.tsx
@@ -12,18 +12,19 @@ interface PortfolioCardProps {
 const PortfolioCard: React.FC<PortfolioCardProps> = ({ project, index }) => {
 
     return (
-        <section className="py-16 bg-gray-50" id="portfolioCard">
-            <div className="container mx-auto px-4 text-center">
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-                    <a
-                        key={index}
-                        href={project.href}
-                        className="bg-white rounded-lg shadow-md p-6"
-                    >
-                        <h3 className="text-xl font-bold mb-2">{project.title}</h3>
-                        <p className="text-gray-600">{project.description}</p>
-                    </a>
+        <section className="py-8 bg-gray-50" id="portfolioCard">
+            <div className="container mx-auto px-2 text-center">
+            <div className="relative group">
+                <img
+                key={index}
+                src={project.href}
+                className="bg-white rounded-lg shadow-md p-4"
+                />
+                <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 flex flex-col justify-center items-center text-white transition-opacity duration-300">
+                <h3 className="text-xl font-bold mb-2">{project.title}</h3>
+                <p className="text-gray-200">{project.description}</p>
                 </div>
+            </div>
             </div>
         </section>
     );
