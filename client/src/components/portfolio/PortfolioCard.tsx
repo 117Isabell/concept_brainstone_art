bd639acbab069d69c04cf9143c75ad7c34f55993
tomi <tomislav.varga@icloud.com>
2024-12-03 14:13:24 +0100

    add: refactored code, added clickable link to image

--- client/src/components/portfolio/PortfolioCard.tsx
+++ client/src/components/portfolio/PortfolioCard.tsx
@@ -14,17 +14,23 @@ const PortfolioCard: React.FC<PortfolioCardProps> = ({ project, index }) => {
     return (
         <section className="py-8 bg-gray-50" id="portfolioCard">
             <div className="container mx-auto px-2 text-center">
-            <div className="relative group">
-                <img
-                key={index}
-                src={project.href}
-                className="bg-white rounded-lg shadow-md p-4"
-                />
-                <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 flex flex-col justify-center items-center text-white transition-opacity duration-300">
-                <h3 className="text-xl font-bold mb-2">{project.title}</h3>
-                <p className="text-gray-200">{project.description}</p>
+                <div className="relative group">
+                    <a href={project.href} target="_blank" rel="noopener noreferrer">
+                        <img
+                            key={index}
+                            src={project.href}
+                            className="bg-white rounded-lg shadow-md 
+                            transition-transform duration-300 transform 
+                            hover:scale-105"
+                        />
+                        <div className="absolute inset-0 bg-black bg-opacity-50 
+                        opacity-0 group-hover:opacity-100 flex flex-col justify-center 
+                        items-center text-white transition-opacity duration-300">
+                            <h3 className="text-xl font-bold mb-2">{project.title}</h3>
+                            <p className="text-gray-200">{project.description}</p>
+                        </div>
+                    </a>
                 </div>
-            </div>
             </div>
         </section>
     );
