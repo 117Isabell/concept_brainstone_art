b7bfc5a79ed3110b0e73124acb1cd601d789ac51
117Isabell <isabellhepburn@outlook.com>
2024-12-09 13:36:46 +0100

    feat : integerate portfolio

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,17 +5,32 @@ import {
   BlueShape,
   GreenShape,
 } from "./components/homepage/AnimatedShapes";
-import PortfolioList from "./components/portfolio/PortfolioList.tsx";
+import PortfolioCard from "./components/portfolio/PortfolioCard";
+import PortfolioList from "./components/portfolio/PortfolioList";
+
 export default function App() {
   return (
-    <div className="relative h-screen w-full bg-white overflow-hidden p-4">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <PurpleShape />
-      <YellowShape />
-      <BlueShape />
-      <GreenShape />
-      <SocialIcons />
-      <PortfolioList />
+    <div>
+      <div className="relative h-screen w-full bg-white overflow-hidden p-4">
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <PurpleShape />
+        <YellowShape />
+        <BlueShape />
+        <GreenShape />
+        <SocialIcons />
+      </div>
+
+      <div>
+        <PortfolioCard
+          project={{
+            title: "Example Project",
+            description: "Project description",
+            href: "https://example.com",
+          }}
+          index={0}
+        />
+        <PortfolioList />
+      </div>
     </div>
   );
 }
