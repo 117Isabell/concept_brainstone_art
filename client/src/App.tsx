43f7fb7217f8a78c7bd76574c2917053ce48b19f
tomi <tomislav.varga@icloud.com>
2024-12-13 10:44:35 +0100

    Revert "add: portfolio component to App.tsx"
    This reverts commit ffa143c00074642100baaacd689f1441cf31b8fb.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import {
   BlueShape,
   GreenShape,
 } from "./components/homepage/AnimatedShapes";
+<<<<<<< HEAD
 import PortfolioCard from "./components/portfolio/PortfolioCard";
 import PortfolioList from "./components/portfolio/PortfolioList";
 
@@ -31,6 +32,17 @@ export default function App() {
         />
         <PortfolioList />
       </div>
+=======
+export default function App() {
+  return (
+    <div className="relative h-screen w-full bg-white overflow-hidden p-4">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <PurpleShape />
+      <YellowShape />
+      <BlueShape />
+      <GreenShape />
+      <SocialIcons />
+>>>>>>> parent of ffa143c (add: portfolio component to App.tsx)
     </div>
   );
 }
